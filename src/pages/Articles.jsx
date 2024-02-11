a8ec9cabc12ddab0c036556f20fbd300ba43adb0
Md. Ridoy Islam <md.ridoyislam@Mds-MacBook-Pro.local>
2024-02-11 14:43:30 +0600

    article page changed

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -2,8 +2,8 @@ import { Button } from "@material-tailwind/react";
 
 export const Articles = () => {
   return (
-    <div className="bg-[#0E0E0E] text-white">
-      <div className="container mx-auto">
+    <div className="bg-[#0E0E0E] text-white overflow-x-hidden">
+      <div className="container mx-auto max-w-screen-2xl">
         <div className="flex justify-between py-4">
           <h2 className="text-xl font-bold">Top News</h2>
           <Button className="rounded-none border border-white">
@@ -11,7 +11,7 @@ export const Articles = () => {
           </Button>
         </div>
 
-        <div className="grid grid-cols-3 gap-16 custom-grid text-center">
+        <div className="grid grid-cols-3 gap-4 custom-grid text-center">
           {/* first row */}
           <div className="space-y-5">
             {/* loop */}
@@ -95,7 +95,7 @@ export const Articles = () => {
               <img
                 src="https://images.unsplash.com/photo-1527192491265-7e15c55b1ed2"
                 alt="ok"
-                className="w-40 h-28 mr-3"
+                className="w-40 h-28 mr-2"
               />
               <h2 className="text-lg my-3">
                 Lorem ipsum dolor, sit amet consectetur adipisicing elit
