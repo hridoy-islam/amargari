acdc96469f01b26917e974170a49b36762186f1d
litansarkar95 <litansarkar95@gmail.com>
2024-03-13 12:17:13 +0600

    profile,service, signup

--- src/pages/CarWash.jsx
+++ src/pages/CarWash.jsx
@@ -3,8 +3,213 @@ import { Link } from "react-router-dom";
 export const CarWash = () => {
   return (
    <>
-   
-   <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+
+<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+
+  <div className="max-w-2xl mx-auto text-center mb-10 lg:mb-14">
+    <h2 className="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white">Pricing</h2>
+    <p className="mt-1 text-gray-600 dark:text-gray-400">Whatever your status, our offers evolve according to your needs.</p>
+  </div>
+
+
+
+
+  <div className="mt-12 grid sm:grid-cols-2 lg:grid-cols-4 gap-6 lg:items-center">
+
+    <div className="flex flex-col border border-gray-200 text-center rounded-xl p-8 dark:border-gray-700">
+      <h4 className="font-medium text-lg text-gray-800 dark:text-gray-200">Free</h4>
+      <span className="mt-7 font-bold text-5xl text-gray-800 dark:text-gray-200">Free</span>
+      <p className="mt-2 text-sm text-gray-500">Forever free</p>
+
+      <ul className="mt-7 space-y-2.5 text-sm">
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            1 user
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Plan features
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Product support
+          </span>
+        </li>
+      </ul>
+
+      <a className="mt-5 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+        Sign up
+      </a>
+    </div>
+
+
+ 
+    <div className="flex flex-col border-2 border-blue-600 text-center shadow-xl rounded-xl p-8 dark:border-blue-700">
+      <p className="mb-3"><span className="inline-flex items-center gap-1.5 py-1.5 px-3 rounded-lg text-xs uppercase font-semibold bg-blue-100 text-blue-800 dark:bg-blue-600 dark:text-white">Most popular</span></p>
+      <h4 className="font-medium text-lg text-gray-800 dark:text-gray-200">Startup</h4>
+      <span className="mt-5 font-bold text-5xl text-gray-800 dark:text-gray-200">
+        <span className="font-bold text-2xl -me-2">$</span>
+        39
+      </span>
+      <p className="mt-2 text-sm text-gray-500">All the basics for starting a new business</p>
+
+      <ul className="mt-7 space-y-2.5 text-sm">
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            2 users
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Plan features
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Product support
+          </span>
+        </li>
+      </ul>
+
+      <a className="mt-5 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="https://github.com/htmlstreamofficial/preline/tree/main/examples/html">
+        Sign up
+      </a>
+    </div>
+
+    <div className="flex flex-col border border-gray-200 text-center rounded-xl p-8 dark:border-gray-700">
+      <h4 className="font-medium text-lg text-gray-800 dark:text-gray-200">Team</h4>
+      <span className="mt-5 font-bold text-5xl text-gray-800 dark:text-gray-200">
+        <span className="font-bold text-2xl -me-2">$</span>
+        89
+      </span>
+      <p className="mt-2 text-sm text-gray-500">Everything you need for a growing business</p>
+
+      <ul className="mt-7 space-y-2.5 text-sm">
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            5 users
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Plan features
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Product support
+          </span>
+        </li>
+      </ul>
+
+      <a className="mt-5 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+        Sign up
+      </a>
+    </div>
+
+    <div className="flex flex-col border border-gray-200 text-center rounded-xl p-8 dark:border-gray-700">
+      <h4 className="font-medium text-lg text-gray-800 dark:text-gray-200">Enterprise</h4>
+      <span className="mt-5 font-bold text-5xl text-gray-800 dark:text-gray-200">
+        <span className="font-bold text-2xl -me-2">$</span>
+        149
+      </span>
+      <p className="mt-2 text-sm text-gray-500">Advanced features for scaling your business</p>
+
+      <ul className="mt-7 space-y-2.5 text-sm">
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            10 users
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Plan features
+          </span>
+        </li>
+
+        <li className="flex space-x-2">
+          <svg className="flex-shrink-0 mt-0.5 size-4 text-blue-600 dark:text-blue-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polyline points="20 6 9 17 4 12"/></svg>
+          <span className="text-gray-800 dark:text-gray-400">
+            Product support
+          </span>
+        </li>
+      </ul>
+
+      <a className="mt-5 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-100 text-blue-800 hover:bg-blue-200 disabled:opacity-50 disabled:pointer-events-none dark:hover:bg-blue-900 dark:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
+        Sign up
+      </a>
+    </div>
+ 
+  </div>
+
+</div>
+
+
+
+<div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <div className="mx-auto max-w-2xl">
+    <div className="text-center">
+      <h2 className="text-xl text-gray-800 font-bold sm:text-3xl dark:text-white">
+      Booking Form
+      </h2>
+    </div>
+
+
+    <div className="mt-5 p-4 relative z-10 bg-white border rounded-xl sm:mt-10 md:p-10 dark:bg-gray-800 dark:border-gray-700">
+      <form>
+        <div className="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-name-1" className="block mb-2 text-sm font-medium dark:text-white">Full name</label>
+          <input type="text" id="hs-feedback-post-comment-name-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Full name" />
+        </div>
+
+        <div className="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-email-1" className="block mb-2 text-sm font-medium dark:text-white">Email address</label>
+          <input type="email" id="hs-feedback-post-comment-email-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Email address" />
+        </div>
+        <div className="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-email-1" className="block mb-2 text-sm font-medium dark:text-white">Package</label>
+          <input type="email" id="hs-feedback-post-comment-email-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Enter Package" />
+        </div>
+
+        <div>
+          <label for="hs-feedback-post-comment-textarea-1" className="block mb-2 text-sm font-medium dark:text-white">Comment</label>
+          <div className="mt-1">
+            <textarea id="hs-feedback-post-comment-textarea-1" name="hs-feedback-post-comment-textarea-1" rows="3" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Leave your comment here..."></textarea>
+          </div>
+        </div>
+
+        <div className="mt-6 grid">
+          <button type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Submit</button>
+        </div>
+      </form>
+    </div>
+
+  </div>
+</div>
+
+
+
+   {/* <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
 
   <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6">
 
@@ -123,7 +328,7 @@ export const CarWash = () => {
 
   </div>
 
-</div>
+</div> */}
    </>
   );
 };
--- src/pages/Consultancy.jsx
+++ src/pages/Consultancy.jsx
@@ -2,67 +2,46 @@ export const Consultancy = () => {
     return (
      <>
      
-     <div className="max-w-5xl px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-        <h2 className="text-center">বাংলাদেশ-এ Car wash</h2>
+     <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <div className="mx-auto max-w-2xl">
+    <div className="text-center">
+      <h2 className="text-xl text-gray-800 font-bold sm:text-3xl dark:text-white">
+      Booking Form
+      </h2>
+    </div>
 
-  <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6">
 
-    <a className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-      <div className="p-6 md:p-5">
-        <div className="flex">
-          <svg className="mt-1 flex-shrink-0 size-5 text-gray-800 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"/><circle cx="9" cy="7" r="4"/><path d="M22 21v-2a4 4 0 0 0-3-3.87"/><path d="M16 3.13a4 4 0 0 1 0 7.75"/></svg>
+    <div className="mt-5 p-4 relative z-10 bg-white border rounded-xl sm:mt-10 md:p-10 dark:bg-gray-800 dark:border-gray-700">
+      <form>
+        <div className="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-name-1" className="block mb-2 text-sm font-medium dark:text-white">Full name</label>
+          <input type="text" id="hs-feedback-post-comment-name-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Full name" />
+        </div>
 
-          <div className="grow ms-5">
-            <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-            Car wash Home service
-            </h3>
-            <p className="text-sm text-gray-500">
-            Car wash Home service Preline users
-            </p>
-            <p>600 TK</p>
+        <div className="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-email-1" className="block mb-2 text-sm font-medium dark:text-white">Email address</label>
+          <input type="email" id="hs-feedback-post-comment-email-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Email address" />
+        </div>
+        <div className="mb-4 sm:mb-8">
+          <label for="hs-feedback-post-comment-email-1" className="block mb-2 text-sm font-medium dark:text-white">Package</label>
+          <input type="email" id="hs-feedback-post-comment-email-1" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Enter Package" />
+        </div>
+
+        <div>
+          <label for="hs-feedback-post-comment-textarea-1" className="block mb-2 text-sm font-medium dark:text-white">Comment</label>
+          <div className="mt-1">
+            <textarea id="hs-feedback-post-comment-textarea-1" name="hs-feedback-post-comment-textarea-1" rows="3" className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" placeholder="Leave your comment here..."></textarea>
           </div>
         </div>
-      </div>
-    </a>
-   
-    <a className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-      <div className="p-6 md:p-5">
-        <div className="flex">
-          <svg className="mt-1 flex-shrink-0 size-5 text-gray-800 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/></svg>
 
-          <div className="grow ms-5">
-            <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-              Get help in the app
-            </h3>
-            <p className="text-sm text-gray-500">
-              Just head to «Help» in the app
-            </p>
-          </div>
+        <div className="mt-6 grid">
+          <button type="submit" className="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">Submit</button>
         </div>
-      </div>
-    </a>
-
-    <a className="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-      <div className="p-4 md:p-5">
-        <div className="flex">
-          <svg className="mt-1 flex-shrink-0 size-5 text-gray-800 dark:text-gray-200" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21.2 8.4c.5.38.8.97.8 1.6v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V10a2 2 0 0 1 .8-1.6l8-6a2 2 0 0 1 2.4 0l8 6Z"/><path d="m22 10-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 10"/></svg>
-
-          <div className="grow ms-5">
-            <h3 className="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
-              Email us
-            </h3>
-            <p className="text-sm text-gray-500">
-              Reach us at <span className="text-blue-600 font-medium dark:text-blue-500">info@site.com</span>
-            </p>
-          </div>
-        </div>
-      </div>
-    </a>
+      </form>
+    </div>
 
   </div>
- 
 </div>
-     
      </>
     );
   };
