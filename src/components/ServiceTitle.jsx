22a30b99b9e171eae4198bbfe3d0a13acb083cfa
Hridoy Islam <hridoy4t@gmail.com>
2024-04-19 22:25:52 +0600

    breadcumb added

--- src/components/ServiceTitle.jsx
+++ src/components/ServiceTitle.jsx
@@ -1,11 +1,18 @@
 /* eslint-disable react/prop-types */
+import bread from "../assets/breadcumb.jpeg";
 export const ServiceTitle = (props) => {
   const { title, description } = props;
   return (
-    <div className="py-10 bg-gray-700">
-      <div className="container mx-auto">
-        <h2 className="text-2xl font-bold text-white">{title}</h2>
-        <p>{description}</p>
+    <div
+      className="relative bg-cover bg-center py-32"
+      style={{
+        backgroundImage: `url(${bread})`,
+      }}
+    >
+      <div className="absolute inset-0 bg-black opacity-70"></div>
+      <div className="container mx-auto relative z-10 text-white">
+        <h2 className="text-4xl font-bold">{title}</h2>
+        <p className="text-2xl font-bold">{description}</p>
       </div>
     </div>
   );
